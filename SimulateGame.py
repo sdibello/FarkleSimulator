652233d3b6b94c8faabeec1ab4adb1bdfd97df59
Steve DiBello <sdibello@hotmail.com>
2021-09-19 06:29:12 -0400

    got reroll working and scoring working, messy though

--- SimulateGame.py
+++ SimulateGame.py
@@ -13,6 +13,7 @@ def gameloop(game):
         for player in game.players:
             player_turn_score = 0
             diceToRoll = 5
+            reRollFlag = True
             print("turn for " + player.name + " passed " + str(len(passedDice)) + "-" + str(passedScore)  )
 
             if passedScore >= player.passedHandlimitScore:
@@ -21,29 +22,54 @@ def gameloop(game):
                 if len(passedDice) == 0:
                     diceToRoll = 5
             
-            turnRolls = rollDice(diceToRoll)
+            while (reRollFlag):
 
-            print(turnRolls)
-            activeRolls = calcScore(turnRolls)
+                print("rolling " + str(diceToRoll))
+                turnRolls = rollDice(diceToRoll)
+                print(turnRolls)
 
-            if (len(activeRolls[1]) == 0):
-                if diceToRoll == 5:
-                    player.BFFarkleCount += 1
-                    print("Big Fat Farkle - (" + str(player.BFFarkleCount) + ")" )
+                activeRolls = calcScore(turnRolls)  ## score, scored-dice, remaining-dice
+                roll_scored_dice = len(activeRolls[1])
+                roll_remaining_dice = len(activeRolls[2])
+                roll_score = activeRolls[0]
+
+
+                if (roll_scored_dice == 0):
+                    if diceToRoll == 5:
+                        player.BFFarkleCount += 1
+                        print("Big Fat Farkle - (" + str(player.BFFarkleCount) + ")" )
+                    else:
+                        player.FarkleCount += 1
+                        print("Farkle - (" + str(player.FarkleCount) + ")" )
+                    player_turn_score = 0
+                    passedDice = [5,5,5,5,5]
+                    passedScore = 0
                 else:
-                    player.FarkleCount += 1
-                    print("Farkle - (" + str(player.FarkleCount) + ")" )
-                player_turn_score = 0
-                passedDice = [5,5,5,5,5]
-                passedScore = 0
-                continue
-            else:
-                player_turn_score = player_turn_score + activeRolls[0]
-                player.score = player.score + player_turn_score
-                print("Player score total - " + str(player_turn_score) + " (" + str(player.score) + ") " + " on " + str(activeRolls[1]))
-                passedScore = player_turn_score
-                passedDice = activeRolls[2]
+                    player_turn_score = player_turn_score + roll_score
+                    print("Player score total - " + str(player_turn_score) + " (" + str(player.score) + ") " + " on " + str(activeRolls[1]))
+                    passedScore = player_turn_score
+                    passedDice = activeRolls[2]
+
+                if (len(passedDice) == 0) and (passedScore > 0):
+                    reRollFlag = True
+                    diceToRoll = 5
+                elif (len(passedDice) == 5) and (passedScore == 0):
+                    reRollFlag = False
+                elif (len(passedDice)) < player.OutPassDiceLimit:
+                    reRollFlag = False
+                    player.score = player.score + player_turn_score
+                    print("score " + str(player_turn_score) + " for a total of " + str(player.score))
+                elif (len(passedDice)) >= player.OutPassDiceLimit:
+                    reRollFlag = True
+                    diceToRoll = len(passedDice)
+                else:
+                    reRollFlag = False
+
         game.calc_highest_score()
+
+def turn_roll(game):
+    # comming soon
+    print("soon")
 
 def main ():
     print("Starting Game")
--- game/Player.py
+++ game/Player.py
@@ -1,10 +1,10 @@
 
 class Player:
-    def __init__(self, name, handlimit, handlimitdice, OutPassLimit):
+    def __init__(self, name, handlimit, handlimitdice, OutPassDiceLimit):
         self.name = name
         self.passedHandlimitScore = handlimit
         self.passedhandimitDice = handlimitdice
-        self.OutPassLimit = OutPassLimit
+        self.OutPassDiceLimit = OutPassDiceLimit
         self.FarkleCount = 0
         self.BFFarkleCount = 0
         self.score = 0
